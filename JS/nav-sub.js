8c593e32957255cb6420312c65a973b4c28e0ef3
ci.macintosh@gmail.com <32034299+cmcntsh@users.noreply.github.com>
2020-04-13 07:22:03 -0600

    nav list formatting

--- JS/nav-sub.js
+++ JS/nav-sub.js
@@ -1,14 +1,14 @@
 document.write('Navigation');
-document.write('<ul>');
+document.write('<ul class="nav-list">');
 document.write('  <li><a href="index.html">Home</a></li>');
 document.write('  <li>');
 document.write('    <input class="menu-group-input" type="checkbox" name ="topics" id="topics">');
 document.write('    <label for="topics">Topics +</label>');
-document.write('    <ul class="menu-group-list">');
+document.write('    <ul class="menu-group-list nav-list">');
 document.write('      <li>');
 document.write('        <input class="menu-group-input" type="checkbox" name ="class_activities" id="class_activities">');
 document.write('        <label for="class_activities">Class Activities +</label>');
-document.write('        <ul class="menu-group-list">');
+document.write('        <ul class="menu-group-list nav-list">');
 document.write('          <li><a href="#0">Empty</a></li>');
 document.write('        </ul>');
 document.write('      </li>');
